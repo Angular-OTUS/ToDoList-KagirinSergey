72a120c01b416eb9eb3c50ae395b9f6757345e69
PARKER\Parker <parkers@mail.ru>
2023-08-31 00:31:57 +0300

    Add Angular Material

---
+++ src/app/material/material.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule } from "@angular/common";
+import { FormsModule } from "@angular/forms";
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ToDoListComponent } from './components/to-do-list/to-do-list.component';
+import { ToDoListItemComponent } from './components/to-do-list/to-do-list-item/to-do-list-item.component';
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ToDoListComponent,
+    ToDoListItemComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    CommonModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
